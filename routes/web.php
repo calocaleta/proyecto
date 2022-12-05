149e2b8ea2ae50526b58a9786f14753318463946
Carlos Alfonso Garcia Gonzales <cgarcia@stsac.com>
2022-12-04 20:24:54 -0500

    Practica en el uso de Vistas

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,6 @@
 <?php
 
-use Illuminate\Http\Request;
+//use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 /**
@@ -12,18 +12,28 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     //return view('welcome');
-    return 'Ruta home';
+    return view('home');
 });
 
 Route::get('blog', function () {
-    return 'Listado de publicaciones';
+    //consulta a base de datos
+    $posts = [
+        ['id' => 1, 'title' => 'PHP', 'slug' => 'php'],
+        ['id' => 2, 'title' => 'Laravel', 'slug' => 'laravel']
+    ];
+
+    return view('blog',['posts' => $posts]);
 });
 
 Route::get('blog/{slug}', function ($slug) {
     //consulta a base de datos
-    return 'Selección: '.$slug;
+    $post = $slug;
+
+    return view('post',['post' => $post]);
 });
 
+/*
 Route::get('buscar', function (Request $request) {
     return $request->all();
 });
+*/
