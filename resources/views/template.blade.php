889e64653d4a9a94082b5f9959381bdf938468e9
Carlos Alfonso Garcia Gonzales <cgarcia@stsac.com>
2022-12-19 22:03:11 -0500

    Creación de Login con Breeze (falta corregir)

--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -8,15 +8,15 @@
 </head>
 <body>
     <p>
-        <a href="{{route('home')}}">Home</a>
-        <a href="{{route('blog')}}">Blog</a>
+		<a href="{{ route('home') }}">Home</a>
+		<a href="{{ route('blog') }}">Blog</a>
 
         @auth
-            <a href="{{ route('dashboard') }}">Dashboard</a>
-        @else
-            <a href="{{ route('login') }}">Login</a>
-        @endauth
-    </p>
+		<a href="{{ route('dashboard') }}">Dashboard</a>
+		@else
+		<a href="{{ route('login') }}">Login</a>
+		@endif
+	</p>
     <hr>
 
     @yield('content')
--- routes/web.php
+++ routes/web.php
@@ -3,14 +3,17 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\PageController;
 
-Route::controller(PageController::class)->group(function (){
 
-    Route::get('/', 'home')->name('home');
-    Route::get('blog', 'blog')->name('blog');
-    Route::get('blog/{post:slug}','post')->name('post');
+Route::controller(PageController::class)->group(function () {     
+
+    Route::get('/',           'home')->name('home');     
+    Route::get('blog',        'blog')->name('blog'); 
+    Route::get('blog/{post:slug}', 'post')->name('post'); 
 
 });
+
 
 Route::get('/dashboard', function () {
     return view('dashboard');
